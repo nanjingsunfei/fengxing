6e68fafedbb9cb48844f5434619f77cd06d8bc61
sunfei08 <sunfei08@meituan.com>
2018-01-07 14:37:48 +0800

    update

--- fengxing-test/src/main/java/cn/njupt/guava/Main.java
+++ fengxing-test/src/main/java/cn/njupt/guava/Main.java
@@ -1,19 +1,18 @@
 package cn.njupt.guava;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by sunfei on 2018/1/7.
  */
 public class Main {
     public static void main(String[] args) {
 
-        Test test = new Test() {
-            @Override
-            public void test(int a, int b) {
-                System.out.println(a + b);
-            }
-        };
-
-        test.test(1, 2);
+        System.out.println(Math.max(1, 2));
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        System.out.println(list.toString());
 
     }
 }
